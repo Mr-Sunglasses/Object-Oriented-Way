06a2cb0bf80f746da522feef9d957ec98ee375bc
Kanishk Pachauri <krishna03012003@icloud.com>
2022-04-22 13:57:23 +0530

    Completed Random and modules import'

---
+++ modules/myModule/Main.py
@@ -0,0 +1,6 @@
+import function as fp
+
+fp.SayHello("Kanishk")
+
+x = fp.Square(122)
+print(x)
---
+++ modules/myModule/function.py
@@ -0,0 +1,6 @@
+def SayHello(name):
+    print(f"Hello {name}")
+
+def Square(number):
+    return number**2
+
